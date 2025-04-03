b6f76b2becec4281e473fe2221b222a6d41912ef
Danilvin <108340803+Danilvin@users.noreply.github.com>
2025-04-03 06:03:10 -0700

    add task2.2 task2.3

---
+++ hw2/task2.2/2.2.cs
@@ -0,0 +1,74 @@
+﻿/* Виноградов Данил ИТВД 2 курс
+ДЗ 2.2 (2⭐) Реализовать подсчет 
+факториала/фибоначчи (рекурсивно и итеративно). 
+Выяснить, с какого номера числа подсчета факториала/фибоначчи 
+рекурсивный вариант заметно медленнее итеративного. .
+*/
+using System.Numerics;
+
+static BigInteger FactorialIter(int n)
+{
+    BigInteger res = BigInteger.One;
+    for (int i = 1; i <= n; i++)
+        res *= i;
+    return res;
+}
+static BigInteger FactorialRecur(int n)
+{
+    if (n == 1)
+        return n;
+    return n * FactorialRecur(n - 1);
+}
+static BigInteger FibonacciIter(int n)
+{
+    if ((n == 0) || (n == 1))
+        return n;
+    BigInteger res = BigInteger.Zero;
+    BigInteger a = BigInteger.Zero;
+    BigInteger b = BigInteger.One;
+    for (int i = 2; i <= n; i++)
+    {
+        res = a+b;
+        a = b;
+        b = res;
+    }
+    return res;
+}
+static BigInteger FibonacciRecur(int n)
+{
+    if ((n == 0) || (n == 1))
+        return n;
+    return FibonacciRecur(n - 1) + FibonacciRecur(n - 2);
+}
+
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{n}! = {FactorialIter(n)}");
+Console.Write("Введите n: ");
+n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{n} элемент последовательности Фибоначчи" +
+                                      $" = {FibonacciIter(n)}");
+
+Console.WriteLine();
+
+var watch = System.Diagnostics.Stopwatch.StartNew();
+FactorialIter(7000);
+watch.Stop();
+var elapsedMs = watch.ElapsedMilliseconds;
+Console.WriteLine($"FactorialIter(7000) - {elapsedMs} ms");
+var watch1 = System.Diagnostics.Stopwatch.StartNew();
+FactorialRecur(7000);
+watch1.Stop();
+var elapsedMs1 = watch1.ElapsedMilliseconds;
+Console.WriteLine($"FactorialRecur(7000) - {elapsedMs1} ms");
+var watch2 = System.Diagnostics.Stopwatch.StartNew();
+FibonacciIter(30);
+watch2.Stop();
+var elapsedMs2 = watch2.ElapsedMilliseconds;
+Console.WriteLine($"FibonacciIter(30) - {elapsedMs2} ms");
+var watch3 = System.Diagnostics.Stopwatch.StartNew();
+FibonacciRecur(30);
+watch3.Stop();
+var elapsedMs3 = watch3.ElapsedMilliseconds;
+Console.WriteLine($"FibonacciRecur(30) - {elapsedMs3} ms");
+
---
+++ hw2/task2.3/2.3.cs
@@ -0,0 +1,29 @@
+﻿/* Виноградов Данил ИТВД 2 курс
+Ввести вещественное число -1 <= x <= 1 и вычислить приближение к e^x, пользуясь формулой
+e^x = 1 + x + (x^2/2!) + (x^3/3!) + ...
+Закончить вычисления, когда очередное слагаемое станет меньше 10^-6
+*/
+using System.Numerics;
+
+static double Factorial(int n)
+{
+    double res = 1;
+    for (int i = 1; i <= n; i++)
+        res *= i;
+    return res;
+}
+static double Exp(double x)
+{
+    double res = 1 + x;
+    int i = 2;
+    while (Math.Pow(x, i)/Factorial(i) >= 0.000001)
+    {
+        res += Math.Pow(x, i) / Factorial(i);
+        i++;
+    }
+    return res;
+}
+
+Console.Write("Введите x: ");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"e^{x} = {Exp(x)}");
